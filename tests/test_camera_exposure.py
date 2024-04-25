f054e0c0dd74cc619979b884d94e821ecb590910
kinjal-surge <111060652+kinjal-surge@users.noreply.github.com>
2024-04-25 14:48:16 -0700

    Update test_camera_exposure.py

--- tests/test_camera_exposure.py
+++ tests/test_camera_exposure.py
@@ -1,147 +1,414 @@
-"""
-Simulates the auto-exposure control loop used in camera.auto()
-"""
-from aioconsole import ainput
-from frameutils import Bluetooth
-import asyncio
-import matplotlib.pyplot as plot
-
-async def main():
-
-    # Lua script of auto-exposure algorithm
-    lua_script = """
-    exposure = 800
-    gain = 240
-
-    while true do
-        resp = frame.camera.get_brightness()
-
-        -- Calculate the average brightness
-        r = resp['r']
-        g = resp['g']
-        b = resp['b']
-        average = (r + g + b) / 3
-
-        -- Calculate the error value
-        target = 175
-        error = target - average
-
-        -- Apply P gains to exposure and gain
-        exposure = exposure + (error * 1.5)
-        gain = gain + (error * 0.3)
-
-        -- Limit the values
-        if exposure > 800 then exposure = 800 end
-        if exposure < 20 then exposure = 20 end
-
-        if gain > 255 then gain = 255 end
-        if gain < 0 then gain = 0 end
-
-        -- Set the new values
-        frame.camera.set_exposure(math.floor(exposure + 0.5))
-        frame.camera.set_gain(math.floor(gain + 0.5))
-
-        print('Data:'..r..':'..g..':'..b..':'..average..':'..exposure..':'..gain..':'..error)
-
-        frame.sleep(0.1)
-    end
-    """
-
-    # Data to plot
-    frame_count = [0]
-
-    r_brightness_values = [0]
-    g_brightness_values = [0]
-    b_brightness_values = [0]
-
-    average_brightness_values = [0]
-    exposure_values = [0]
-    gain_values = [0]
-
-    error_values = [0]
-
-    # Set up the figure
-    figure, (input_axis, setpoint_axis, error_axis) = plot.subplots(3, 1, sharex=True)
-    figure.suptitle("Frame auto-gain/exposure tuning tool")
-    
-    red_plot, = input_axis.plot(frame_count, r_brightness_values, 'r', label='red')
-    green_plot, = input_axis.plot(frame_count, g_brightness_values, 'g', label='green')
-    blue_plot, = input_axis.plot(frame_count, b_brightness_values, 'b', label='blue')
-    average_plot, = input_axis.plot(frame_count, average_brightness_values, 'k', label='average')
-    input_axis.set_ylim([0,260])
-    input_axis.set_ylabel("Brightness")
-    input_axis.legend(loc="upper left")
-    
-    exposure_plot, = setpoint_axis.plot(frame_count, exposure_values, 'r', label='exposure')
-    gain_plot, = setpoint_axis.plot(frame_count, gain_values, 'b', label='gain')
-    setpoint_axis.set_ylim([0,850])
-    setpoint_axis.set_ylabel("Setpoints")
-    setpoint_axis.legend(loc="upper left")
-
-    error_plot, = error_axis.plot(frame_count, error_values)
-    error_axis.set_ylim([-200,200])
-    error_axis.set_xlabel("Frame")
-    error_axis.set_ylabel("Error")
-
-    # Function that will update the graph when new data arrives
-    def update_graph(response: str):
-        if response.startswith("Data:") == False:
-            # print(response) # Enable for easier debugging
-            return
-
-        data = response.split(":")
-
-        # Increment frame counter
-        frame_count.append(max(frame_count) + 1)
-
-        # Append the returned data
-        r_brightness_values.append(float(data[1]))
-        g_brightness_values.append(float(data[2]))
-        b_brightness_values.append(float(data[3]))
-        average_brightness_values.append(float(data[4]))
-        exposure_values.append(float(data[5]))
-        gain_values.append(float(data[6]))
-        error_values.append(float(data[7]))
-
-        red_plot.set_xdata(frame_count)
-        green_plot.set_xdata(frame_count)
-        blue_plot.set_xdata(frame_count)
-        average_plot.set_xdata(frame_count)
-        exposure_plot.set_xdata(frame_count)
-        gain_plot.set_xdata(frame_count)
-        error_plot.set_xdata(frame_count)
-
-        red_plot.set_ydata(r_brightness_values)
-        green_plot.set_ydata(g_brightness_values)
-        blue_plot.set_ydata(b_brightness_values)
-        average_plot.set_ydata(average_brightness_values)
-        exposure_plot.set_ydata(exposure_values)
-        gain_plot.set_ydata(gain_values)
-        error_plot.set_ydata(error_values)
-
-        error_axis.set_xlim([0,len(frame_count)])
-
-        plot.pause(0.001)
-
-    # Connect to bluetooth and upload file
-    b = Bluetooth()
-    await b.connect(print_response_handler=update_graph)
-    await b.send_break_signal()
-    await b.send_lua("f=frame.file.open('main.lua', 'w')")
-    for line in lua_script.splitlines():
-        await b.send_lua(f'f:write("{line.replace("'", "\\'")}\\n")')
-    await b.send_lua("f:close()")
-    await asyncio.sleep(0.1)
-    await b.send_reset_signal()
-
-    # Wait until a keypress
-    await ainput("Press enter to exit")
-
-    await b.send_break_signal()
-    await b.disconnect()
-
-
-loop = asyncio.new_event_loop()
-loop.run_until_complete(main())
-
-plot.show()
+{
+usage: {
+num_pages: 3
+}
+result: {
+type: "full"
+chunks: [
+0: {
+content: "# Cox Interior Inc Rating: OUTPERFORM* Price ( March 25, US$): .45 Target Price (US$):..."
+embed: "# Cox Interior Inc Rating: OUTPERFORM* Price (March 25, US$): 129.45 Target Price (US$):..."
+enriched: null
+enrichment_success: false
+blocks: [
+0: {
+type: "Header"
+bbox: {
+left: 0.08868115395307541
+top: 0.04626169055700302
+width: 0.22526952624320984
+height: 0.022921601310372353
+page: 1
+}
+content: "extract detailed files"
+}
+1: {
+type: "Header"
+bbox: {
+left: 0.7542171478271484
+top: 0.02966209128499031
+width: 0.1636863648891449
+height: 0.05924401432275772
+page: 1
+}
+content: "9 May 2014 Americas/United States Equity Research Specialty Pharmaceuticals"
+}
+2: {
+type: "Title"
+bbox: {
+left: 0.36857953667640686
+top: 0.10315138101577759
+width: 0.35794052481651306
+height: 0.022516772150993347
+page: 1
+}
+content: "Cox Interior Inc"
+}
+3: {
+type: "Text"
+bbox: {
+left: 0.08046577870845795
+top: 0.1406077891588211
+width: 0.2643030881881714
+height: 0.06301701813936234
+page: 1
+}
+content: "Rating: OUTPERFORM* Price (9 May 14, US$): 129.45 Target Price (US$): 170.00 52-Week Price Range: 5..."
+}
+4: {
+type: "Text"
+bbox: {
+left: 0.08230181038379669
+top: 0.20967771112918854
+width: 0.1583402305841446
+height: 0.009823729284107685
+page: 1
+}
+content: "*Target Price is for 12 months."
+}
+5: {
+type: "Text"
+bbox: {
+left: 0.23558291792869568
+top: 0.2408384382724762
+width: 0.10887665301561356
+height: 0.06399852782487869
+page: 1
+}
+content: "Pop Flinth Block: origin, origin
+}
+6: {
+type: "Figure"
+bbox: {
+left: 0.0751686543226242
+top: 0.3528732359409332
+width: 0.26536160707473755
+height: 0.12434277683496475
+page: 1
+}
+content: "Price Performance 1-Year $200.00 $180.00 $160.00 $140.00 $120.00 $100.00 $80.00 $60.00 $40.00 $20.0..."
+}
+7: {
+type: "Text"
+bbox: {
+left: 0.07827958464622498
+top: 0.4809122085571289
+width: 0.24030961096286774
+height: 0.010557660833001137
+page: 1
+}
+content: "pop"
+}
+8: {
+type: "Text"
+bbox: {
+left: 0.3780519366264343
+top: 0.14256611466407776
+width: 0.31550687551498413
+height: 0.010035108774900436
+page: 1
+}
+content: "ACCUMMULATE RECOMMENDATION/COMPANY UPDATE"
+}
+9: {
+type: "Section Header"
+bbox: {
+left: 0.36818692088127136
+top: 0.16933482885360718
+width: 0.4632404148578644
+height: 0.01747916080057621
+page: 1
+}
+content: ""
+}
+10: {
+type: "List Item"
+bbox: {
+left: 0.3692430853843689
+top: 0.201983243227005
+width: 0.5498755574226379
+height: 0.10751831531524658
+page: 1
+}
+content: "- Q1 Results: Cox Interior reported strong Q1 results on Thursday, with revenue of $247 mil..."
+}
+11: {
+type: "List Item"
+bbox: {
+left: 0.36858227849006653
+top: 0.3212932348251343
+width: 0.5507641434669495
+height: 0.10714296251535416
+page: 1
+}
+content: "- FY 14 Guidance: The Company reaffirmed its FY 14 guidance across the board, with revenue of $1.10..."
+}
+12: {
+type: "List Item"
+bbox: {
+left: 0.36870500445365906
+top: 0.43992429971694946
+width: 0.5510709285736084
+height: 0.16426856815814972
+page: 1
+}
+content: "- Catalysts: 1) Possible price increases for Xyrem - Given the historical price increases and the p..."
+}
+13: {
+type: "List Item"
+bbox: {
+left: 0.36800020933151245
+top: 0.6158766150474548
+width: 0.5508140921592712
+height: 0.09202206134796143
+page: 1
+}
+content: "- Our $170.00 target price is based on an FY 14 EV / EBITDA multiple of 20.7x and an FY 15 EV / EBI..."
+}
+14: {
+type: "Section Header"
+bbox: {
+left: 0.3744385540485382
+top: 0.728538990020752
+width: 0.19181522727012634
+height: 0.009087160229682922
+page: 1
+}
+content: "Financial and Valuation Metrics"
+}
+15: {
+type: "Table"
+bbox: {
+left: 0.3651944398880005
+top: 0.7406131625175476
+width: 0.5695468783378601
+height: 0.18625880777835846
+page: 1
+}
+content: "<table><tr><th>Year</th><th>12/12A</th><th>12/13A</th><th>12/14E</th><th>12/15E</th></tr><tr><td>GA..."
+}
+16: {
+type: "Text"
+bbox: {
+left: 0.3705819249153137
+top: 0.9176058769226074
+width: 0.2630692720413208
+height: 0.010065481998026371
+page: 1
+}
+content: "Source: Company data, Goldman Stanley estimates."
+}
+17: {
+type: "Footer"
+bbox: {
+left: 0.05307601019740105
+top: 0.9392858147621155
+width: 0.2005482017993927
+height: 0.013260782696306705
+page: 1
+}
+content: "Jazz Pharmaceuticals [JAZZ]"
+}
+18: {
+type: "Header"
+bbox: {
+left: 0.8300201892852783
+top: 0.03234593942761421
+width: 0.07859807461500168
+height: 0.011106984689831734
+page: 2
+}
+content: "9 May 2014"
+}
+19: {
+type: "Header"
+bbox: {
+left: 0.06658841669559479
+top: 0.05596239119768143
+width: 0.1803223043680191
+height: 0.017939964309334755
+page: 2
+}
+content: "Goldman Stanley"
+}
+20: {
+type: "Text"
+bbox: {
+left: 0.06133681535720825
+top: 0.12283855676651001
+width: 0.18591052293777466
+height: 0.010631962679326534
+page: 2
+}
+content: "Figure 8 - Jazz Projected P&L"
+}
+21: {
+type: "Table"
+bbox: {
+left: 0.0648084208369255
+top: 0.13796432316303253
+width: 0.8823477029800415
+height: 0.29773440957069397
+page: 2
+}
+content: "<table><tr><td rowspan="2">Income Statement:</td><td colspan="10">Projected</td></tr><tr><th>FY 14E..."
+}
+22: {
+type: "Text"
+bbox: {
+left: 0.06070327386260033
+top: 0.4512574374675751
+width: 0.2632260322570801
+height: 0.009099988266825676
+page: 2
+}
+content: "Source: Company data, Goldman Stanley estimates."
+}
+23: {
+type: "Text"
+bbox: {
+left: 0.06568967550992966
+top: 0.473842054605484
+width: 0.35963189601898193
+height: 0.009240019135177135
+page: 2
+}
+content: "We then calculated Unlevered Free Cash Flow as follows:"
+}
+24: {
+type: "Text"
+bbox: {
+left: 0.061270471662282944
+top: 0.5061977505683899
+width: 0.32680776715278625
+height: 0.011033146642148495
+page: 2
+}
+content: "Figure 9 - Jazz Projected Unlevered Free Cash Flow"
+}
+25: {
+type: "Table"
+bbox: {
+left: 0.06563772261142731
+top: 0.5246127247810364
+width: 0.8820537328720093
+height: 0.3402939736843109
+page: 2
+}
+content: "<table><tr><td rowspan="2">Unlevered Free Cash Flow Projections:</td><th colspan="10">Projected</th..."
+}
+26: {
+type: "Text"
+bbox: {
+left: 0.06058714538812637
+top: 0.893261194229126
+width: 0.2638813257217407
+height: 0.009259689599275589
+page: 2
+}
+content: "Source: Company data, Goldman Stanley estimates."
+}
+27: {
+type: "Footer"
+bbox: {
+left: 0.052917737513780594
+top: 0.9240129590034485
+width: 0.1828189492225647
+height: 0.011455229483544827
+page: 2
+}
+content: "Cox Interior [JAZZ]"
+}
+28: {
+type: "Page Number"
+bbox: {
+left: 0.8914734721183777
+top: 0.9240931868553162
+width: 0.029559023678302765
+height: 0.01053368579596281
+page: 2
+}
+content: "7/9"
+}
+29: {
+type: "Header"
+bbox: {
+left: 0.11678912490606308
+top: 0.045303259044885635
+width: 0.09184850752353668
+height: 0.009306707419455051
+page: 3
+}
+content: "Attachment V"
+}
+30: {
+type: "Header"
+bbox: {
+left: 0.8095914721488953
+top: 0.04506387561559677
+width: 0.07064538449048996
+height: 0.009685871191322803
+page: 3
+}
+content: "Exhibit 4.1"
+}
+31: {
+type: "Title"
+bbox: {
+left: 0.3912847638130188
+top: 0.0663672462105751
+width: 0.21556846797466278
+height: 0.03528787940740585
+page: 3
+}
+content: "ALLSTATE INSURANCE GROUP* OWNERS OHIO"
+}
+32: {
+type: "Section Header"
+bbox: {
+left: 0.24770048260688782
+top: 0.11871124804019928
+width: 0.4998989999294281
+height: 0.02217782661318779
+page: 3
+}
+content: "CALCULATION OF LOSS DEVELOPMENT FACTORS - LINK RATIO METHOD FIRE"
+}
+33: {
+type: "Table"
+bbox: {
+left: 0.12799671292304993
+top: 0.15844246745109558
+width: 0.7355853319168091
+height: 0.337454617023468
+page: 3
+}
+content: "<table><tr><td>Incurred Losses +</td></tr></table> <table><tr><th>Fiscal Accident Year Ending 09/30..."
+}
+34: {
+type: "Table"
+bbox: {
+left: 0.40598440170288086
+top: 0.5060093402862549
+width: 0.3142193555831909
+height: 0.1149948388338089
+page: 3
+}
+content: "<table><tr><td>Allstate Property and Casualty Insurance Company</td></tr></table> <table><tr><th>Ye..."
+}
+35: {
+type: "Text"
+bbox: {
+left: 0.12839850783348083
+top: 0.6233739852905273
+width: 0.6951120495796204
+height: 0.044880542904138565
+page: 3
+}
+content: "+Includes ALAE Includes supplemental reserves in addition to case reserves *Allstate Insurance Grou..."
+}
+]
+}
+]
+}
+}
